b2f3857bcd5406c8b873c53724a03aed9dcf053c
Dakota Willingham <kota@vaga.tk>
2020-02-18 23:38:16 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,7 @@
 def find_element_index(array, value_to_find)
   array.length.times do |index|
     if array.include?(value_to_find) == array[index]
-      return array[index]
+      return index
     end
   end
 end
