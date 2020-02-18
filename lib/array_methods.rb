7bc7eaff81a63c644ff3a46a5aa1cba9b6e20bee
Dakota Willingham <kota@vaga.tk>
2020-02-18 23:37:00 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,5 +1,8 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  array.length.times do |index|
+  if array.include?(value_to_find) == array[index]
+    array[index]
+  end
 end
 
 def find_max_value(array)
