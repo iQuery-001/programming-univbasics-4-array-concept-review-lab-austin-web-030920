9710cd642c1a934ef76d17ee4c31be759a7e7caf
Dakota Willingham <kota@vaga.tk>
2020-02-18 23:47:17 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -13,7 +13,7 @@ def find_max_value(array)
   highest = 0
   array.length.times do |index|
     if array[index] > highest
-      highest = array[index]]
+      highest = array[index]
     end
   end
 end
